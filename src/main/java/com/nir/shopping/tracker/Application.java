7e0434a84dd202003f6b4dc6f44f44a0e440c063
Nirmith Anawaratne <nirmith.akash@gmail.com>
2025-04-12 15:22:48 +0530

    chore: Initial commit, required files from the template
    Include the Spring Web, JPA, PostgreSQL plugins with DevTools

---
+++ src/main/java/com/nir/shopping/tracker/Application.java
@@ -0,0 +1,13 @@
+package com.nir.shopping.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/nir/shopping/tracker/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nir.shopping.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
